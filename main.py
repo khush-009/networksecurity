a12bd48940404804427652695df642f8e339047a
khush-009 <khushsharma002@gmail.com>
2025-06-26 17:36:19 +0530

    Model Creation as well as fastapi end point created

--- main.py
+++ main.py
@@ -1,36 +1,52 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+
+ 
+
 import sys
 
 if __name__=='__main__':
     try:
-        training_pipeline_config = TrainingPipelineConfig()
-
-        data_ingestion_config = DataIngestionConfig(training_pipeline_config)
-        data_ingestion = DataIngestion(data_ingestion_config)
+        trainingpipelineconfig=TrainingPipelineConfig()
+        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion=DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
-        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-        print(data_ingestion_artifact)
-        logging.info('Data initiation completed')
+        dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
+        print(dataingestionartifact)
+        print('\n')
 
-        data_validation_config = DataValidationConfig(training_pipeline_config)
-        data_validation = DataValidation(data_ingestion_artifact,data_validation_config)
-        logging.info('Initialte the data validation')
-        data_validation_artifact = data_validation.initiate_data_validation()
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
         print(data_validation_artifact)
-        logging.info('Data validation completed')
-        
-        data_transformation_config = DataTransformationConfig(training_pipeline_config)
-        data_transformation = DataTransformation(data_validation_artifact,data_transformation_config)
-        logging.info('Data Transformation started')
-        data_transformation_artifact = data_transformation.initiate_data_transformation()
-        print(data_transformation_artifact)
-        logging.info('Data Transformation completed')
+        print('\n')
 
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data Transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        logging.info("data Transformation completed")
+        print(data_transformation_artifact)
+        print('\n')
+
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        logging.info('Model Training Started')
+        model_trainer = ModelTrainer(model_trainer_config,data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info('Model Training completed')
+        print(model_trainer_artifact)
+        print('\n')
+        
+        
+            
     except Exception as e:
-        raise NetworkSecurityException(e, sys)
+           raise NetworkSecurityException(e,sys)
